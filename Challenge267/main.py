eea2022289170f4ffd020c298fa800df28753cc4
matteofilia <matteo.compsci@gmail.com>
2016-05-17 17:16:53 -0400

    Fixed minor off by one error

--- Challenge267/main.py
+++ Challenge267/main.py
@@ -20,7 +20,7 @@ place = int(input("Please enter the place your dog got: "))
 maxPlaces = int(input("Please enter the total number of dogs: "))
 
 for i in range(0, maxPlaces):
-	if(i != place):
+	if(i+1 != place):
 		sys.stdout.write(str(i+1))
 		sys.stdout.write(str(getNumberEnding(i+1)))
 		sys.stdout.write(", ")
